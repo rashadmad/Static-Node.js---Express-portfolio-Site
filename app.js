e4d58ff772aacda06a38db606d96fd3f43459aaa
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-06-10 19:05:15 -0700

    set up a route

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//tell the app to use pug
+app.set('view engine', 'pug');
+
+app.get('/home', (req, res) => {
+    res.send("<h1>I Love Treehouse!</h1>");
+  });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
