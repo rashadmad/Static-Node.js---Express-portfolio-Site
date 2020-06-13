3455a3887ebf597cec4a12d5c062697de55a1845
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-06-12 21:17:49 -0700

    add data into a varible

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-Parser');
 const app = express();
 const port = 3000;
+const data = require('./data.json') 
 
 //tell the app to use pug
 app.set('view engine', 'pug');
@@ -11,4 +12,4 @@ app.get('/', (req, res) => {
     res.send('afsgsdfgs');
 });
 
-app.listen(port, () => console.log(`You app is up and running on http://localhost:${port}`))
+app.listen(port, () => console.log(`You app is up and running on http://localhost:${port}` + data))
