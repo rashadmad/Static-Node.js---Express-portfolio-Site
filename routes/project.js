e620c7f7abf3303193dcb73920f3fccf7c4919c6
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-06-17 00:54:56 -0700

    get descriptions on projects displaying

--- routes/project.js
+++ routes/project.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const { projects } = require('../data.json');
 
 //sets up a route for the project page and adds dynamic data to it
-router.get('/', (req, res) => {
-    res.locals.name = projects[0].project_name;
-    res.locals.description = projects[0].project_description;
-    res.locals.items = projects[0].technologies;
-    res.locals.imageA = projects[0].image_urls[0];
-    res.locals.imageB = projects[0].image_urls[1];
-    res.locals.imageC = projects[0].image_urls[0];
+router.get('/:id', (req, res) => {
+    res.locals.name = projects[req.params.id].project_name;
+    res.locals.description = projects[req.params.id].description;
+    res.locals.items = projects[req.params.id].technologies;
+    res.locals.imageA = projects[req.params.id].image_urls[0];
+    res.locals.imageB = projects[req.params.id].image_urls[1];
+    res.locals.imageC = projects[req.params.id].image_urls[0];
+    res.locals.liveUrl = projects[req.params.id].live_link;
+    res.locals.githubUrl = projects[req.params.id].github_link;
     res.render('project');
 });
 
